f1272e0f60644658eaf9d842ca5c201567e89d50
BabyWoody <jmrosillo17@gmail.com>
2020-02-25 10:47:07 -0800

    =added javascript files

---
+++ practice/Javascript/week 6/first.js
@@ -0,0 +1,23 @@
+console.log(10 * 10);
+
+console.log(1000);
+
+console.log(69);
+//strings are always in single or double quotes
+console.log('Jose Rosillo 650');
+
+console.log('Hello, "how" are you?');
+
+console.log('Nan' );
+// order of operations
+console.log(100 / (10 * 5));
+
+//This is not math, the + can be used for addiion or concactination:
+console.log("How are you " + 'Jose');
+
+//The Boolean datatype
+console.log(true);
+console.log(false);
+
+console.log(1);
+console.log(0);
---
+++ practice/Javascript/week 6/second.js
@@ -0,0 +1,36 @@
+// Variables Intro
+
+console.log(10 + 10);
+console.log(10 + 21);
+
+var x;
+
+x="Hello";
+
+console.log(x);
+
+
+var money;
+
+money=100;
+
+
+// Single Line Comment
+
+/*Multi
+Line
+Comment
+*/
+
+
+console.log(money);
+console.log(money + money);
+//  Calling a variable coolVar, and taking its value and passing it to console.log, which will send it to the console.
+
+var coolVar;
+var coolVar = 1000;
+coolVar = 2000;
+console.log(coolVar);
+
+
+// Make sure spell your variable calls correctly, otherwise...
