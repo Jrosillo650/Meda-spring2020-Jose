b8ca5b3aa2e93c4f6e2031d452caf0cfe4fca6b7
BabyWoody <jmrosillo17@gmail.com>
2020-03-02 11:46:45 -0800

    =added forloop file

---
+++ practice/Javascript/week 6/loops.js
@@ -0,0 +1,11 @@
+console.log("Testing");
+
+var number=1;
+//While loop"while (condition) {code}"
+//console.log(number):
+
+// Do While loop "do {} while ();"
+
+//A loop needs a counter so we can track how many times it has loops.
+
+
