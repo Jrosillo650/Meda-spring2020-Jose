34f746c1059f5fe67cbbe8fafe631be67dc25abd
BabyWoody <jmrosillo17@gmail.com>
2020-03-02 12:51:04 -0800

    =Portfolio Version 1.0

---
+++ practice/Javascript/week 7/arrays.js
@@ -0,0 +1,13 @@
+var myVar = "One Item"
+var firstday = "Monday"
+var secondday= "Tuesday"
+
+//Array syntax, a collection of data that is surrounded b square brackets.
+
+var daysOfWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+
+// You can mix datatypes in an Array.
+//{100,true, "hello"};
+
+
+
